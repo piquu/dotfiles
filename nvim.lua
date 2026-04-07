d6f5b7576fef0d72b81f4eb44897feef4d1283ea
piquu <l.smole@protonmail.com>
2026-04-07 18:12:59 +0000

    replace oil with mini files

--- nvim.lua
+++ nvim.lua
@@ -19,7 +19,7 @@ vim.pack.add({
   { src = "https://github.com/neovim/nvim-lspconfig" },
   { src = "https://github.com/stevearc/conform.nvim" },
   { src = "https://github.com/mason-org/mason.nvim" },
-  { src = "https://github.com/stevearc/oil.nvim" },
+  { src = "https://github.com/nvim-mini/mini.files" },
   { src = "https://github.com/nvim-mini/mini.pick" },
 })
 
@@ -81,13 +81,18 @@ require("conform").setup({
 
 require("mason").setup()
 
-require("oil").setup({
-  default_file_explorer = true,
-  view_options = {
-    show_hidden = true,
+require("mini.files").setup({
+  content = {
+    prefix = function(fs_entry)
+      if fs_entry.fs_type == "directory" then
+        return "d ", "MiniFilesDirectory"
+      else
+        return ". ", "MiniFilesFile"
+      end
+    end,
   },
 })
-vim.keymap.set("n", "\\", "<cmd>Oil<cr>")
+vim.keymap.set("n", "\\", MiniFiles.open)
 
 require("mini.pick").setup()
 vim.keymap.set("n", "<leader>s", "<cmd>Pick files<cr>")
